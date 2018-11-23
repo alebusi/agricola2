122f9411776fdabd0549958f5c58db76c2a8608c
alebusi <allebusi@gmail.com>
2018-11-23 16:50:11 +0100

    Update counter.js

--- counter.js
+++ counter.js
@@ -4,7 +4,7 @@ var tess = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 function myfunc(id,i,ind) {
   tess[ind]+=i;
   v=tess[ind];
-  if (v == 0) {
+  if (v === 0) {
     document.getElementById(id).innerHTML = "-";
   }
   else { 
@@ -33,30 +33,61 @@ function myfunc(id,i,ind) {
 
 function myfunc2(div) {
   var className = div.getAttribute("class");
+  if (document.getElementById("controllo").className=="off") {
   if (className=="spazio") {
     div.className = "stanza";
     }
-  else if (className=="stanza") {
+  if (className=="stanza") {
+    div.className = "stalla";
+    }
+  else if (className=="stalla") {
     div.className = "campo";
     }
   else if (className=="campo") {
-    div.className = "pascolo";
+    div.className = "grano_3";
+    }
+  else if (className=="grano_3") {
+    div.className = "grano_2";
+    }
+  else if (className=="grano_2") {
+    div.className = "grano_1";
+    }
+  else if (className=="grano_1") {
+    div.className = "ortaggio_2";
+    }
+  else if (className=="ortaggio_2") {
+    div.className = "ortaggio_1";
     }
   else {
     div.className = "spazio";
   }
   var suona = new Audio('campo.mp3');
   suona.play();
+  }
 }
 
 function mysel(div) {
-  var className = div.getAttribute("class");
-  if (className=="largo") {
-    div.className = "largo_nero";
-    }
-  else if (className=="largo_nero") {
-    div.className = "largo";
-    }
   var suona = new Audio('campo.mp3');
+  var className = div.getAttribute("class");
+  if (document.getElementById("controllo").className=="on") {
+    if (className=="largo") {
+       div.className = "largo_nero";
+       }
+    else if (className=="largo_nero") {
+       div.className = "largo";
+       }
+    suona.play();
+    }
+}
+
+function mySwitch(div) {
+  var suona = new Audio('campo.mp3');
+  var className = div.getAttribute("class");
+  if (className=="off") {
+    div.className = "on";
+    }
+  else if (className=="on") {
+    div.className = "off";
+    }
   suona.play();
 }
