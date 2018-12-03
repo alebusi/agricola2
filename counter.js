76c5d118d388208108316e289475c4a8f9202420
alebusi <allebusi@gmail.com>
2018-12-03 17:57:41 +0100

    Update counter.js

--- counter.js
+++ counter.js
@@ -33,33 +33,40 @@ function myfunc(id,i,ind) {
 
 function myfunc2(div) {
   var className = div.getAttribute("class");
-  if (document.getElementById("controllo").className=="off") {
-  if (className=="spazio") {
-    div.className = "stanza";
-    }
-  else if (className=="stanza") {
-    div.className = "stalla";
-    }
-  else if (className=="stalla") {
-    div.className = "campo";
-    }
-  else if (className=="campo") {
-    div.className = "grano_3";
-    }
-  else if (className=="grano_3") {
-    div.className = "grano_2";
-    }
-  else if (className=="grano_2") {
-    div.className = "grano_1";
-    }
-  else if (className=="grano_1") {
-    div.className = "ortaggio_2";
-    }
-  else if (className=="ortaggio_2") {
-    div.className = "ortaggio_1";
-    }
-  else {
-    div.className = "spazio";
+  if (document.getElementById("controllo").className=="standard") { 
+    if (className=="spazio") {
+      div.className = "campo";
+      }
+    else if (className=="campo") {
+      div.className = "grano_3";
+      }
+    else if (className=="grano_3") {
+      div.className = "grano_2";
+      }
+    else if (className=="grano_2") {
+      div.className = "grano_1";
+      }
+    else if (className=="grano_1") {
+      div.className = "ortaggio_2";
+      }
+    else if (className=="ortaggio_2") {
+      div.className = "ortaggio_1";
+      }
+    else {
+      div.className = "spazio";
+  else if (document.getElementById("controllo").className=="edilizia") {
+    if (className=="spazio") {
+      div.className = "stanza";
+      }
+    else if (className=="stanza") {
+      div.className = "stalla";
+      } 
+    else if (className=="stalla") {
+      div.className = "stanza_argilla";
+      }
+    else if (className=="stanza_argilla") {
+      div.className = "stanza_pietra";
+      }
   }
   var suona = new Audio('campo.mp3');
   suona.play();
@@ -69,7 +76,7 @@ function myfunc2(div) {
 function mysel(div) {
   var suona = new Audio('campo.mp3');
   var className = div.getAttribute("class");
-  if (document.getElementById("controllo").className=="on") {
+  if (document.getElementById("controllo").className=="steccati") {
     if (className=="largo") {
        div.className = "largo_nero";
        }
@@ -83,11 +90,14 @@ function mysel(div) {
 function mySwitch(div) {
   var suona = new Audio('campo.mp3');
   var className = div.getAttribute("class");
-  if (className=="off") {
-    div.className = "on";
+  if (className=="standard") {
+    div.className = "steccati";
     }
-  else if (className=="on") {
-    div.className = "off";
+  else if (className=="steccati") {
+    div.className = "edilizia";
+    }
+  else if (className=="edilizia") {
+    div.className = "standard";
     }
   suona.play();
 }
